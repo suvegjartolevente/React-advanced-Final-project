dbb419e300ba7ec39766ab4cd59fa86265cfc1fb
Levente <suvegjartolevente@gmail.com>
2025-06-14 18:14:26 +0200

    Time format fixed

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import { Heading } from "@chakra-ui/react";
+import { useLoaderData } from "react-router-dom";
 
-export const loader = async()=>{
+export const loader = async () => {
   const eventsRes = await fetch(`http://localhost:3000/events`);
 
-const events = await eventsRes.json();
-return{
-  events
+  const events = await eventsRes.json();
+
+  return {
+    events,
+  };
 };
-
-};
-
-
 
 export const EventsPage = () => {
-  const {events} = useLoaderData();
-  return (
-  <div className="events-list">
-<Heading>List of events</Heading>
-{events.map((event)=>(
-  <div key={event.id} className="event">
-    <h2>{event.title}</h2>
-    <p>{event.description}</p>
-    <img src={event.image} alt={event.name}></img>
-    <p>From: {event.startTime} to: {event.endTime}</p>
-  </div>
-) )}
+  const { events } = useLoaderData();
 
-  </div>);
+  return (
+    <div className="events-list">
+      <Heading>List of events</Heading>
+      {events.map((event) => {
+        const getTime = (dateStr) =>
+          new Date(dateStr).toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+
+        const dateOnly = event.startTime.split("T")[0];
+        const fixedStartTime = getTime(event.startTime);
+        const fixedEndTime = getTime(event.endTime);
+        return (
+          <div key={event.id} className="event">
+            <h2>{event.title}</h2>
+            <p>{event.description}</p>
+            <img src={event.image} alt={event.name}></img>
+            <p>
+              date: {dateOnly}
+              From: {fixedStartTime} to: {fixedEndTime}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
